bab3b876ccc8fedcd1c2e41bb2b6e1e778a0e50d
Your Name <you@example.com>
2024-05-16 21:01:47 -0300

    feat: paginas e componentes

---
+++ src/pages/Home/Main/index.js
@@ -0,0 +1,42 @@
+import { useState, useEffect } from "react";
+import api from "services/api";
+
+const Main = ({ content }) => {
+  const [user, setUser] = useState([]);
+
+  useEffect(() => {
+    if (content) {
+      api.get("/user/" + content.id_user).then((Response) => {
+        setUser(Response.data);
+      });
+    }
+  }, []);
+  return (
+    <>
+      <div className="py-3 bb-black ">
+        <h6 className="color-gray">{content.date}</h6>
+        <h6 className="uppercase color-primary">{content.category}</h6>
+        <a href="" className="link-title">
+          <h4 className="mt-1">{content.title}</h4>
+        </a>
+
+        <p className="mt-1">{content.resume}</p>
+        <div className="mt-2 flex-space">
+          <div className="flex-start-row">
+            <div className="profile">
+              <img src="profile/ny.jpg" className="profile-img" alt="" />
+            </div>
+            <div className="ml-2">
+              <h6 className="color-primary">
+                {user.name} {user.surname}
+              </h6>
+              <h6 className="color-gray">{user.user}</h6>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Main;
---
+++ src/pages/Home/index.js
@@ -0,0 +1,77 @@
+import Bunner from "./Banner";
+import Card from "./Card";
+import Hero from "./Hero";
+import Main from "./Main";
+
+import star from "svg/icon-star.svg";
+
+import { useState, useEffect } from "react";
+import api from "services/api";
+
+const Home = () => {
+  const [main, setMain] = useState([]);
+  const [mostseen, setmostseen] = useState([]);
+  const [Banner, setBanner] = useState([]);
+
+  useEffect(() => {
+    api.get("/posts?star=5&_limit=2").then((result) => {
+      setMain(result.data);
+    });
+
+    api.get("/posts?_sort=date&_order=desc&_limit=1").then((result) => {
+      setBanner(result.data);
+    });
+    api.get("/posts?_sort=views&_order=desc&_limit=3").then((result) => {
+      setmostseen(result.data);
+    });
+  }, []);
+
+  return (
+    <>
+      <Hero />
+
+      <section className="container">
+        <div className="row">
+          <div className="grid-5 pt-5 pb-3">
+            <img src={star} className="icon-l" alt="" />
+            <h2 className="mt-1">Os melhores e mais bem votados do mês</h2>
+
+            <p className="mt-2">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ornare
+              urna pharetra ut ac, pellentesque.
+            </p>
+            <div className="mt-2 flex-space">
+              <div className="flex-start-row">
+                <div className="profile">
+                  <img src="profile/ny.jpg" className="profile-img" alt="" />
+                </div>
+                <div className="ml-2">
+                  <h6 className="color-primary">Násser Yousef Ali</h6>
+                  <h6 className="color-gray">Author</h6>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="grid-7">
+            {main.map((item) => {
+              return <Main key={item.id} content={item} />;
+            })}
+          </div>
+        </div>
+      </section>
+
+      <div className="bg-section">
+        <section className="container">
+          <h3 className="ml-2 mb-3">Mais vistos</h3>
+          <div className="row">
+            <Card />
+            <Card />
+            <Card />
+          </div>
+        </section>
+      </div>
+    </>
+  );
+};
+
+export default Home;
